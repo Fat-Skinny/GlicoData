631f9dd432acd6b4ee076e4820b5ace76bdffa68
Pedro Jose Berbert Bruno <pbruno@cit>
2016-04-28 14:08:56 -0300

    Colocando o projeto no GitHub

---
+++ platforms/android/assets/www/js/Controllers/registerManagerController.js
@@ -0,0 +1,3 @@
+app.controller('registerManagerController', ['$scope','DBService', function ($scope,DBService) {
+    $scope.rPeriod = "BF";
+}]);
---
+++ platforms/android/assets/www/js/Services/DBService.js
@@ -0,0 +1,26 @@
+app.service('DBService',['$q',function($q){
+    DBServiceMethods = [];
+    db = window.sqlitePlugin.openDatabase({name: "GlicoData.db", location: "default", androidLockWorkaround: 1});
+
+    DBServiceMethods.createTables = function(){
+        db.transaction(function(tx){
+            tx.executeSql("CREATE TABLE IF NOT EXISTS GLICO_DATA(ID INTEGER PRIMARY KEY,"
+                         +"R_D VARCHAR(10),"
+                         +"BF_T VARCHAR(5),BF_G INT,BF_C INT,"
+                         +"M_L_T VARCHAR(5),M_L_G INT,M_L_C INT,"
+                         +"L_T VARCHAR(5),L_G INT,L_C INT,"
+                         +"AN_L_T VARCHAR(5),AN_L_G INT,AN_L_C INT,"
+                         +"D_T VARCHAR(5),D_G INT,D_C INT)");
+
+            tx.executeSql("CREATE TABLE IF NOT EXISTS TEMP_GLICO_DATA("
+                         +"R_D VARCHAR(10),"
+                         +"BF_T VARCHAR(5),BF_G INT,BF_C INT,"
+                         +"M_L_T VARCHAR(5),M_L_G INT,M_L_C INT,"
+                         +"L_T VARCHAR(5),L_G INT,L_C INT,"
+                         +"AN_L_T VARCHAR(5),AN_L_G INT,AN_L_C INT,"
+                         +"D_T VARCHAR(5),D_G INT,D_C INT)");
+        });
+    };
+
+    return DBServiceMethods;
+}]);
